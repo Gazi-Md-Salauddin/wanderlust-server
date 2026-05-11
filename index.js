ff0b4b43c889efbebb66887690ec48b7987b798f
Gazi-Md-Salauddin <mdsalauddin329132@gmail.com>
2026-05-11 21:28:21 +0600

    delete function built

--- index.js
+++ index.js
@@ -56,6 +56,12 @@ async function run() {
         res.json(result)
     })
     
+    app.delete('/destination/:id', async(req, res) => {
+      const {id} = req.params
+      const result = await destinationCollection.deleteOne({_id: new ObjectId(id)})
+      res.json(result)
+    })
+    
     
     
     await client.db("admin").command({ ping: 1 });
